0090482512ce840d7c2c49667f47a644322dad3f
Ilia Denisov <denilv@ya.ru>
2021-06-10 18:12:32 +0300

    [up]: update training script

--- repos/mrc-for-flat-nested-ner/scripts/reproduce/ru_ner_test.sh
+++ repos/mrc-for-flat-nested-ner/scripts/reproduce/ru_ner_test.sh
@@ -11,12 +11,11 @@ MAXLEN=128
 MAXNORM=1.0
 
 OUTPUT_DIR="./logs/mrc-test-run/ru_ner_test_wwmlarge_sgd_warm${WARMUP}lr${LR}_drop${MRC_DROPOUT}_norm${MAXNORM}_bsz32_gold_span_weight${SPAN_WEIGHT}_warmup${WARMUP}_maxlen${MAXLEN}_continue"
-CHECKPOINT="/home/denilv/projects/mrc-for-flat-nested-ner/logs/mrc-test-run/ru_ner_test_wwmlarge_sgd_warm0lr1e-5_drop0.4_norm1.0_bsz32_gold_span_weight0.1_warmup0_maxlen128/epoch=2_v2.ckpt"
+# CHECKPOINT="/home/denilv/projects/mrc-for-flat-nested-ner/logs/mrc-test-run/ru_ner_test_wwmlarge_sgd_warm0lr1e-5_drop0.4_norm1.0_bsz32_gold_span_weight0.1_warmup0_maxlen128/epoch=2_v2.ckpt"
 mkdir -p $OUTPUT_DIR
 
 python trainer.py \
 --data_dir $DATA_DIR \
---pretrained_checkpoint $CHECKPOINT \
 --bert_config_dir $BERT_DIR \
 --max_length $MAXLEN \
 --batch_size 6 \
@@ -30,7 +29,7 @@ python trainer.py \
 --default_root_dir $OUTPUT_DIR \
 --mrc_dropout $MRC_DROPOUT \
 --bert_dropout $BERT_DROPOUT \
---max_epochs 5 \
+--max_epochs 25 \
 --span_loss_candidates "pred_and_gold" \
 --weight_span $SPAN_WEIGHT \
 --warmup_steps $WARMUP \
